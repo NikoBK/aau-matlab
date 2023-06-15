0a037fb007383ad342ce8c4e6d65524bf6055447
NikoBK <112914618+NikoBK@users.noreply.github.com>
2023-06-15 21:45:49 +0200

    least squares fit

---
+++ algebra/trilaterationLeastSquares.m
@@ -0,0 +1,13 @@
+% Name:     trilaterationLeastSquares.m
+% Created:  6/15/2023
+% Author:   nikobk
+
+% ABOUT
+% Find the least squares solution (u) for the equation:
+% Xy = vHat so that ||v - vHat||^2 is minimized.
+
+% Clear cache and console.
+clear; clc; close all;
+
+% Placeholder, replace this with values.
+PLACEHOLDER = 10;
