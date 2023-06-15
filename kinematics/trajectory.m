addc58458943897a032f0d62317af5882ba82df8
NikoBK <112914618+NikoBK@users.noreply.github.com>
2023-06-15 22:12:40 +0200

    robotic kinematics (rmms) scripts

---
+++ kinematics/trajectory.m
@@ -0,0 +1,10 @@
+% Name:     trajectory.m
+% Created:  5/29/2023
+% Author:   NikoBK
+
+% Clear cache and console.
+clear; clc; close all;
+
+disp("If you are reading this in your command window it means you have pressed 'Run'.")
+disp("Please use 'Run Section' while being in the correct section within the code instead.")
+return;
