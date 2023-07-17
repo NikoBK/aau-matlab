3b7e22d86b4c9ce8e995c1133c6f72cfe01974d0
NikoBK <112914618+NikoBK@users.noreply.github.com>
2023-07-17 18:10:27 +0200

    add jacobian

--- kinematics/trajectory.m
+++ kinematics/trajectory.m
@@ -129,7 +129,24 @@ end
 plot(t, theta);
 
 
+%% Jacobians
+% The usage of syms is for MATLAB to allow undefined variables.
+syms x y z L1 d2 theta theta2 e
 
+% Usage:
+% jacobian([Your functions], [var's to diff. in relation to])
+%
+% Constructor:
+% elem1 = x^3 * y^2 - 5 * x^2 * y^2;
+% elem2 = y^6 - 3 * y^3 * x + 7;
+% jacboian([elem1, elem2], [x, y, z]) 
+
+jacobian([ ...
+    L1 * cos(theta) + d2 * sin(theta), ...
+    L1 * sin(theta) - d2 * cos(theta), ...
+    theta - theta2 ...
+    ], ...
+    [ theta, d2, theta2])
 
 
 
